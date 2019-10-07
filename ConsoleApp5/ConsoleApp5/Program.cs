b6c98cbaee12d3e289f501c60b31ed6a5a32760e
Elev <Elev@TEB123003>
2019-10-07 12:23:41 +0200

    Gjort if loop till inlämningen

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Gissa ett tal mellan 1-100");
+
+            string stNr = Console.ReadLine();
+            int tal1 = int.Parse(stNr);
+            if (tal1 < 2)
+                Console.WriteLine("Du gissa för lågt, försök igen");           
+            else if (tal1 > 2)
+                Console.WriteLine("Du gissa för högt, försök igen");
+            else
+                Console.WriteLine("Grattis du gissade rätt");
+
         }
     }
 }
