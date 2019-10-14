067a7e4eaeef726a0f1e06dae984c7fa1126bfd8
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-10-14 11:59:45 +0200

    skrivit pseudokod

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -4,29 +4,51 @@ namespace ConsoleApp5
 {
     class Program
     {
+        /*
+         * Först skriver datorn ut vad användaren ska göra (Gissa ett tal mellan 1-100) 
+         * Datorn väljer ett slumpat mellan 1-100
+         * Loopen börjar
+         * användaren gissar ett tal
+         * Antal gissningar ökar +1
+         * Antal gissningar skrivs
+         * om gissningen > slumpat tal skriv ut "du gissa för högt"
+         * om gissninge < slumpat tal skriv ut "du gissa för lågt"
+         * om gissningen == slumpat tal skriv ut "grattis du gissa rätt"
+         * Loopen avslutar när gissningen = slumpal
+         * programmet skriver ut antal gissningar
+         * Programmet avslutas
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Gissa ett tal mellan 1-100");
-           Random random = new Random();           
-           int randomN = random.Next(1, 101);
-            int
+            Console.WriteLine("Gissa ett tal mellan 1-100"); 
+            Random random = new Random();
+            int randomN = random.Next(1, 101);
+            int Gissa;
+            int Antalgissningar = 0;
 
-            int tal1;
 
             do
             {
-                string stNr = Console.ReadLine(); 
-                tal1 = int.Parse(stNr);
-                if (tal1 < randomN)
-                    Console.WriteLine("Du gissa för lågt, försök igen");
-                else if (tal1 > randomN)
-                    Console.WriteLine("Du gissa för högt, försök igen");
+                Gissa = int.Parse(Console.ReadLine()); 
 
-            } while (tal1 != randomN);
+                if (Gissa < randomN)
+                {
+                    Console.WriteLine("Du gissa för lågt, försök igen");
+                    Console.Write("Antal gissningar ");
+                }
+                else if (Gissa > randomN)
+                {
+                    Console.WriteLine("Du gissa för högt, försök igen");
+                    Console.Write("Antal gissningar ");
+                }
+                Console.WriteLine(++Antalgissningar);
+               
+            } while (Gissa != randomN);
             
 
                 Console.WriteLine("Grattis du gissade rätt");
-            
+            Console.Write("antal gissningar ");
+            Console.WriteLine(Antalgissningar++);
         }
     }
 }
